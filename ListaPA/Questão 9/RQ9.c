21f1537799e6b8ec6a2b2ca37be956ca5fb504b6
rennanf <54864972+rennanf@users.noreply.github.com>
2021-06-26 14:45:15 -0300

    Create RQ9.c

---
+++ ListaPA/Questão 9/RQ9.c
@@ -0,0 +1,33 @@
+Seja x um vetor de 4 elementos, declarado da forma TIPO x[4];. Suponha que depois da declaração, x esteja armazenado no endereço de memória 4092 (ou seja, o endereço de x[0]).
+suponha também que na máquina seja usada uma variável do tipo char ocupa 1 byte, do tipo int ocupa 2 bytes, do tipo float ocupa 4 bytes e do tipo double ocupa 8 bytes.
+Quais serão os valores de x+1, x+2 e x+3 se: 
+
+
+◦ x for declarado como char? ◦ 
+
+char pos 0 = 0x7ffd82da0f28
+char pos 1 = 0x7ffd82da0f29
+char pos 2 = 0x7ffd82da0f2a
+char pos 0 = 0x7ffd82da0f2b
+
+x for declarado como int? 
+
+ int pos 0= 0x7ffd82da0f10
+ int pos 1= 0x7ffd82da0f14
+ int pos 2= 0x7ffd82da0f18  
+ int pos 3= 0x7ffd82da0f1c
+    
+    
+ x for declarado como float? 
+   
+ float pos 0= 0x7ffd82da0f00
+ float pos 1= 0x7ffd82da0f04
+ float pos 2= 0x7ffd82da0f08
+ float pos 3= 0x7ffd82da0f0c
+     
+ x for declarado como double?
+   
+ double pos 0 = 0x7ffd82da0ee0
+ double pos 0 = 0x7ffd82da0ee8
+ double pos 0 = 0x7ffd82da0ef0
+ double pos 0 = 0x7ffd82da0ef8
