5f504a6dd9f82ed0656775367efb1db5dfc02325
rennanf <54864972+rennanf@users.noreply.github.com>
2021-06-26 14:12:02 -0300

    Create RQ6.c

---
+++ ListaPA/Questão 6/RQ6.c
@@ -0,0 +1,12 @@
+Assumindo  que  pulo[]  é  um  vetor  do  tipo  int,  quais  das  seguintes  expressões  referenciam  ovalor do terceiro elemento do vetor?
+
+
+
+
+*(pulo + 2); : Após acessar o  operador de referência, somamos duas unidades e assim acessamos o valor do terceiro elemento.
+*(pulo + 4); : Não tem acesso ao terceiro elemento
+pulo + 4: Não tem acesso ao terceiro elemento
+pulo + 2; : Não tem acesso ao terceiro elemento
+
+
+Logo, *(pulo + 2) é a expressão correta para acessar o terceiro elemento.
